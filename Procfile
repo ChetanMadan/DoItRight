caee48aaf223d99a744ba58ce4b17a7254374561
Chetan Madan <chetanmadan24@gmail.com>
2018-12-13 19:46:17 +0530

    herokutry

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python run.py
+web: python test_website.py
--- test_website.py
+++ test_website.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index ():
-    return finalmost_nolag.main()
+    return enter.main()
 
 @app.route('/v')
 def index1 ():
