8e613c98886943a4c1e60a30e3ac668e1bf0cdca
Chetan Madan <chetanmadan24@gmail.com>
2019-02-08 03:40:59 +0530

    working website fix

--- enter.py
+++ enter.py
@@ -1,13 +1,13 @@
 import media
 import fresh_tomatoes
 
-def main():
-    push_ups = media.Movie("Push Up","Story about a boy whose toys come to life","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon1.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon1.gif")
-    #print(toy_story.storyline)
-    plank = media.Movie("Plank","Marine on alien planet","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon2.png","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon2.gif")
-    #print(avatar.storyline)
-    #avatar.show_trailer()
-    crunches = media.Movie("Crunches","Chipmunks who can sing","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon3_new.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon4.gif")
-    toe_touch = media.Movie("Toe Touch","Using rock music to learn","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon4_new.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon3.gif")
-    exercises = [push_ups,plank,crunches,toe_touch]
-    fresh_tomatoes.open_movies_page(exercises)
+
+push_ups = media.Movie("Push Up","Story about a boy whose toys come to life","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon1.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon1.gif")
+#print(toy_story.storyline)
+plank = media.Movie("Plank","Marine on alien planet","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon2.png","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon2.gif")
+#print(avatar.storyline)
+#avatar.show_trailer()
+crunches = media.Movie("Crunches","Chipmunks who can sing","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon3_new.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon4.gif")
+toe_touch = media.Movie("Toe Touch","Using rock music to learn","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon4_new.jpg","file:///home/dexter/Desktop/projects/ml/DoItRight/msgifs/icon3.gif")
+exercises = [push_ups,plank,crunches,toe_touch]
+fresh_tomatoes.open_movies_page(exercises)
--- test_website.py
+++ test_website.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index ():
-    return enter.main()
+    return enter
 
 @app.route('/v')
 def index1 ():
