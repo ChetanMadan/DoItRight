74b00785d1008da4295ae042a413df464fa9761f
Chetan Madan <chetanmadan24@gmail.com>
2019-02-16 12:16:41 +0530

    error beep done

--- test_website.py
+++ test_website.py
@@ -11,15 +11,10 @@ def index ():
 exercises = ['push_ups','plank','crunches','toe_touch']
 
 
-@app.route('/<key>')
-def index1 (key):
-    to_dict={
-        'push_ups':'icon1.gif',
-        'plank':'icon2.gif',
-        'crunches':'icon3.gif',
-        'toe_touch':'icon4.gif'
-    }
-    return fin_comparison_working.main('msgifs/'+to_dict[key])
+@app.route('/v')
+def index1 ():
+    
+    return fin_comparison_working.main('exer.mp4')
 
 
 if __name__ == ("__main__"):
